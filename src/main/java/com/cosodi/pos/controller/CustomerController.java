b07bdd8107fd81619f8074b478e695d10990d4d0
Luis Guisado <lguisadom@gmail.com>
2023-05-03 23:40:33 -0500

    feat: Spring Hateoas

--- src/main/java/com/cosodi/pos/controller/CustomerController.java
+++ src/main/java/com/cosodi/pos/controller/CustomerController.java
@@ -11,6 +11,8 @@ import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +21,9 @@ import com.cosodi.pos.dto.CustomerDTO;
 import com.cosodi.pos.entity.Customer;
 import com.cosodi.pos.service.ICustomerService;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/api/v1/customers")
@@ -64,6 +69,35 @@ public class CustomerController {
 	public ResponseEntity<Void> deleteById(@PathVariable("id") Long id) {
 		this.iCustomerService.deleteById(id);
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+	@GetMapping("/hateoas/{id}")
+	public EntityModel<CustomerDTO> findByIdHateoas(@PathVariable Long id) {
+		CustomerDTO customerDTO = this.convertToDTO(iCustomerService.findById(id));
+		EntityModel<CustomerDTO> resource = EntityModel.of(customerDTO);
+
+		WebMvcLinkBuilder link1 = linkTo(methodOn(this.getClass()).findById(id));
+		resource.add(link1.withRel("customer-info"));
+
+		WebMvcLinkBuilder link2 = linkTo(methodOn(DocumentTypeController.class).findById(customerDTO.getDocumentTypeId()));
+		resource.add(link2.withRel("document-type-info"));
+
+		WebMvcLinkBuilder link3 = linkTo(methodOn(PersonTypeController.class).findById(customerDTO.getPersonTypeId()));
+		resource.add(link3.withRel("person-type-info"));
+
+		WebMvcLinkBuilder link4 = linkTo(methodOn(GenderController.class).findById(customerDTO.getGenderId()));
+		resource.add(link4.withRel("gender-info"));
+
+		WebMvcLinkBuilder link5 = linkTo(methodOn(DepartmentController.class).findById(customerDTO.getDepartmentId()));
+		resource.add(link5.withRel("department-info"));
+
+		WebMvcLinkBuilder link6 = linkTo(methodOn(ProvinceController.class).findById(customerDTO.getProvinceId()));
+		resource.add(link6.withRel("province-info"));
+
+		WebMvcLinkBuilder link7 = linkTo(methodOn(DistrictController.class).findById(customerDTO.getDistrictId()));
+		resource.add(link7.withRel("district-info"));
+
+		return resource;
 	}
 
 	private Customer convertToEntity(CustomerDTO customerDTO) {
